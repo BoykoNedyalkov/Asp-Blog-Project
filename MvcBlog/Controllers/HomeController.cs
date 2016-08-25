fa1e053035bfc39d9688e8f2c43632847a197dc2
kvladimirov <kosyo.85@abv.bg>
2016-08-25 22:41:24 +0300

    Added ActionResult Contacts in HomeController

--- MvcBlog/Controllers/HomeController.cs
+++ MvcBlog/Controllers/HomeController.cs
@@ -23,6 +23,42 @@ namespace MvcBlog.Controllers
         {
             return View();
         }
+
+        [HttpPost]      
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contacts(ContactForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("recipient@gmail.com")); 
+                message.From = new MailAddress("sender@outlook.com");
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "teambik2016@gmail.com",
+                        Password = "project123"  
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+        public ActionResult Sent()
+        {
+            return View();
+        }
         public ActionResult References()
         {
             return View();
