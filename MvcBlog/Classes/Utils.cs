5872b0adebfc6bc27de02c57f8ae1215fecbe5b7
IMinchevQA <i.minchev.qa@gmail.com>
2016-08-31 13:44:23 +0300

    Posts text limited to 200 characters

---
+++ MvcBlog/Classes/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcBlog.Classes
+{
+    public class Utils
+    {
+        public static string CutText(string text, int maxLen = 200)
+        {
+            if (text == null)
+            {
+                return null;
+            }
+            if (text.Length > maxLen)
+            {
+                return text.Substring(0, maxLen) + "..."; 
+            }
+            return text;
+        }
+    }
+}
